90d22bb5746b6cca8b0d681aa7df2f39b749739b
loewy-moon <loewy@jejesoft.com>
2024-09-29 16:25:49 +0900

    0929

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
 import './styles/common.scss';
 import './styles/_color.scss';
-import Index from './pages/Index'
-import Sub1 from './pages/Sub1';
-import Sub2 from './pages/Sub2';
+import SiteHeader from './components/SiteHeader';
+import SiteMap from './components/SiteMap';
+import SiteFooter from './components/SiteFooter';
+import Index  from './pages/Index'
+import Sub1   from './pages/Sub1';
+import Sub2   from './pages/Sub2';
 function App() {
   return (
-    <Routes>
-      <Route path="/"     element={<Index />} />
-      <Route path="/Sub1" element={<Sub1 />} />
-      <Route path="/Sub2" element={<Sub2 />} />
-    </Routes>
+    <>
+      <SiteHeader/>
+      <Routes>
+        <Route path="/"     element={<Index />} />
+        <Route path="/Sub1" element={<Sub1 />} />
+        <Route path="/Sub2" element={<Sub2 />} />
+      </Routes>
+      <SiteMap/>
+      <SiteFooter/>
+    </>
   );
 }
 
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
-import SiteHeader from '../components/SiteHeader';
+
 import Main from '../components/index/Main';
 import Portfolio from '../components/index/Portfolio';
 import Invoice from '../components/index/Invoice';
 import Lesson from '../components/index/Lesson';
-import SiteMap from '../components/SiteMap';
-import SiteFooter from '../components/SiteFooter';
+
 const Index =()=>{
   return(
     <>
-      <SiteHeader/>
       <Main/>
       <Portfolio/>
       <Invoice/>
       <Lesson/>
-      <SiteMap/>
-      <SiteFooter/>
     </>
   );
 }
